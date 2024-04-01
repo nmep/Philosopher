5d77dde1c062b7938910106badc7c38000bd1357
lgarfi <lyess15@sfr.fr>
2024-04-01 15:29:16 +0200

    qwer

--- src/philo_action/ft_philo.c
+++ src/philo_action/ft_philo.c
@@ -6,26 +6,25 @@
 /*   By: lgarfi <lgarfi@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/26 10:45:49 by lgarfi            #+#    #+#             */
-/*   Updated: 2024/03/30 17:27:09 by lgarfi           ###   ########.fr       */
+/*   Updated: 2024/03/31 11:34:28 by lgarfi           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo.h"
 
-int	ft_philo(t_philo *philo, t_fork_lr *current_fork_pose)
+inline int	ft_philo(t_philo *philo, t_fork_lr *current_fork_pose)
 {
-	ft_think(philo, current_fork_pose->l_fork->fork_n);
 	if (current_fork_pose->l_fork->fork_n < current_fork_pose->r_fork->fork_n)
 	{
 		ft_eat(philo, &philo->mutex.tab_fork[current_fork_pose->l_fork->fork_n],
 			&philo->mutex.tab_fork[current_fork_pose->r_fork->fork_n], current_fork_pose);
-		ft_sleep(current_fork_pose->l_fork->fork_n, philo);
 	}
 	else
 	{
 		ft_eat(philo, &philo->mutex.tab_fork[current_fork_pose->r_fork->fork_n],
 				&philo->mutex.tab_fork[current_fork_pose->l_fork->fork_n], current_fork_pose);
-		ft_sleep(current_fork_pose->l_fork->fork_n, philo);
 	}
+	ft_sleep(current_fork_pose->l_fork->fork_n, philo);
+	ft_think(philo, current_fork_pose->l_fork->fork_n);
 	return (1);
 }
--- src/philo_action/routine_tools.c
+++ src/philo_action/routine_tools.c
@@ -6,61 +6,74 @@
 /*   By: lgarfi <lgarfi@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/25 16:44:02 by lgarfi            #+#    #+#             */
-/*   Updated: 2024/03/30 21:04:00 by lgarfi           ###   ########.fr       */
+/*   Updated: 2024/03/31 12:04:45 by lgarfi           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo.h"
 
-void	ft_usleep(int time_to_usleep, struct timeval start, struct timeval end)
+long	ft_print_time(void)
 {
-	long int elapsed;
+	struct timeval	t;
+	long			res;
 
-	elapsed = time_to_usleep - (((end.tv_sec - start.tv_sec) * 1000000000L + (end.tv_usec - start.tv_usec)));
-	printf("tw = %ld\n", elapsed);
-	usleep(elapsed);
+	gettimeofday(&t, NULL);
+	res = (t.tv_sec * 1000 + t.tv_usec * 0.001) * 0.000001;
+	// printf("%ld res\n", res);
+	return ((t.tv_sec * 1000) + (t.tv_usec * 0.001));
 }
 
-void	ft_think(t_philo *philo, int philo_n)
+long	ft_timestamp(void)
+{
+	struct timeval t;
+
+	gettimeofday(&t, NULL);
+	return ((t.tv_sec * 1000) + (t.tv_usec * 0.001));
+}
+
+void	ft_usleep(int elapsed)
+{
+	long	time;
+
+	time = ft_timestamp();
+	while (1)
+	{
+		if (ft_timestamp() - time >= elapsed)
+			break ;
+		usleep(1);
+	}
+}
+
+inline void	ft_think(t_philo *philo, int philo_n)
 {
 	pthread_mutex_lock(&philo->mutex.print);
 	gettimeofday(&philo->time.timestamp, NULL);
-	printf("%ld %d is thinking\n", philo->time.timestamp.tv_usec, philo_n);
+	printf("%ld %d is thinking\n", ft_print_time(), philo_n);
 	pthread_mutex_unlock(&philo->mutex.print);
 }
 
-void ft_sleep(int philo_n, t_philo *philo)
+inline void ft_sleep(int philo_n, t_philo *philo)
 {
 	pthread_mutex_lock(&philo->mutex.print);
 	gettimeofday(&philo->time.start, NULL);
 	gettimeofday(&philo->time.timestamp, NULL);
-	philo->time.micro_time = philo->time.timestamp.tv_usec;
-	printf("%ld %d is sleeping\n", philo->time.timestamp.tv_usec, philo_n);
-	pthread_mutex_unlock(&philo->mutex.print);
-	// usleep(philo->ph_data.time_to_sleep);
+	printf("%ld %d is sleeping\n", ft_print_time(), philo_n);
 	gettimeofday(&philo->time.end, NULL);
-	ft_usleep(philo->ph_data.time_to_sleep, philo->time.start, philo->time.end);
+	pthread_mutex_unlock(&philo->mutex.print);
+	ft_usleep(philo->ph_data.time_to_sleep);
 }
 
-void ft_eat(t_philo *philo, pthread_mutex_t *first_lock, pthread_mutex_t *second_lock, t_fork_lr *current_pose)
+inline void ft_eat(t_philo *philo, pthread_mutex_t *first_lock, pthread_mutex_t *second_lock, t_fork_lr *current_pose)
 {
 	pthread_mutex_lock(first_lock);
 	pthread_mutex_lock(second_lock);
 	pthread_mutex_lock(&philo->mutex.print);
-	gettimeofday(&philo->time.start, NULL);
 	gettimeofday(&philo->time.timestamp, NULL);
-	printf("%ld %d has taken a fork\n", philo->time.timestamp.tv_usec, current_pose->l_fork->fork_n);
+	printf("%ld %d has taken a fork\n", ft_print_time(), current_pose->l_fork->fork_n);
 	gettimeofday(&philo->time.timestamp, NULL);
-	printf("%ld %d is eating\n", philo->time.timestamp.tv_usec, current_pose->l_fork->fork_n);
+	printf("%ld %d is eating\n", ft_print_time(), current_pose->l_fork->fork_n);
 	pthread_mutex_unlock(&philo->mutex.print);
-	gettimeofday(&philo->time.end, NULL);
-	ft_usleep(philo->ph_data.time_to_eat, philo->time.start, philo->time.end);
+	ft_usleep(philo->ph_data.time_to_eat);
 	pthread_mutex_unlock(first_lock);
 	pthread_mutex_unlock(second_lock);
 }
-
-// prendre le temps en milli seconde
-// le convertir en micro seconde
-// le comparer avec time to die
-// si le resulat est positif alors c'est bon
-// sinon le philo est considere comme mort
